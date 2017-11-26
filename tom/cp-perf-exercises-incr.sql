53183e596d303888abecb9278d77c570aa01f601
Tom W. Yedwab <tom@khanacademy.org>
2017-11-25 22:31:42 -0800

    Convert one query to standard SQL

--- tom/cp-perf-exercises-incr.sql
+++ tom/cp-perf-exercises-incr.sql
@@ -4,77 +4,93 @@ SELECT
   edit.week AS week,
 
   # Quartiles of time to load editing exercise data, in seconds
-  edit.p5_time AS load_secs_p5,
-  edit.q1_time AS load_secs_q1,
-  edit.median_time AS load_secs_median,
-  edit.q3_time AS load_secs_q3,
-  edit.p95_time AS load_secs_p95,
+  edit.times[OFFSET(1)] AS load_secs_p5,
+  edit.times[OFFSET(6)] AS load_secs_q1,
+  edit.times[OFFSET(10)] AS load_secs_median,
+  edit.times[OFFSET(16)] AS load_secs_q3,
+  edit.times[OFFSET(20)] AS load_secs_p95,
 
   # Status of attempts to load editing exercise data
-  es.attempts AS load_attempts,
-  es.server_errors AS load_server_errors,
+  editstatus.attempts AS load_attempts,
+  editstatus.server_errors AS load_server_errors,
 
   # Quartiles of time to save exercise items, in seconds
-  item.p5_time AS item_save_secs_p5,
-  item.q1_time AS item_save_secs_q1,
-  item.median_time AS item_save_secs_median,
-  item.q3_time AS item_save_secs_q3,
-  item.p95_time AS item_save_secs_p95,
+  item.times[OFFSET(1)] AS item_save_secs_p5,
+  item.times[OFFSET(6)] AS item_save_secs_q1,
+  item.times[OFFSET(10)] AS item_save_secs_median,
+  item.times[OFFSET(16)] AS item_save_secs_q3,
+  item.times[OFFSET(20)] AS item_save_secs_p95,
 
   # Status of attempts to save exercise items
-  is.attempts AS item_save_attempts,
-  is.server_errors AS item_save_server_errors
+  itemstatus.attempts AS item_save_attempts,
+  itemstatus.server_errors AS item_save_server_errors
 FROM
 
 # Time taken by API endpoint for loading an exercise for editing
 (SELECT
-    STRFTIME_UTC_USEC(UTC_USEC_TO_WEEK(INTEGER(start_time*1000000), 1), "%Y-%m-%d") AS week,
-    NTH(5, QUANTILES(end_time-start_time,101)) AS p5_time,
-    NTH(25, QUANTILES(end_time-start_time,101)) AS q1_time,
-    NTH(50, QUANTILES(end_time-start_time,101)) AS median_time,
-    NTH(75, QUANTILES(end_time-start_time,101)) AS q3_time,
-    NTH(95, QUANTILES(end_time-start_time,101)) AS p95_time
-    FROM TABLE_DATE_RANGE(logs.requestlogs_, USEC_TO_TIMESTAMP(UTC_USEC_TO_WEEK(NOW(), 1)-1000000*60*60*24*14), CURRENT_TIMESTAMP())
-    WHERE REGEXP_MATCH(resource, '/api/internal/exercises/[x1-9a-f]+/edit') AND method = 'GET' AND status = 200
+    FORMAT_DATE("%Y-%m-%d", DATE_TRUNC(DATE_FROM_UNIX_DATE(CAST(start_time/(60*60*24) AS INT64)), WEEK)) AS week,
+    APPROX_QUANTILES(end_time - start_time, 20) AS times
+    FROM `logs.requestlogs_*`
+    WHERE
+        REGEXP_CONTAINS(resource, r'^/api/internal/exercises/[x0-9a-f]+/edit$')
+        AND method = 'GET'
+        AND status = 200
+        AND _TABLE_SUFFIX BETWEEN 
+            FORMAT_DATE("%Y%m%d", DATE_SUB(CURRENT_DATE(), INTERVAL 14 DAY))
+            AND FORMAT_DATE("%Y%m%d", DATE(TIMESTAMP "2017-10-01 00:00:00"))
     GROUP BY week) edit
 
-LEFT JOIN EACH
+LEFT JOIN
 
 # Status for API endpoint for loading an exercise for editing
 (SELECT
-    STRFTIME_UTC_USEC(UTC_USEC_TO_WEEK(INTEGER(start_time*1000000), 1), "%Y-%m-%d") AS week,
+    FORMAT_DATE("%Y-%m-%d", DATE_TRUNC(DATE_FROM_UNIX_DATE(CAST(start_time/(60*60*24) AS INT64)), WEEK)) AS week,
     COUNT(*) AS attempts,
-    SUM(status = 500) AS server_errors,
-    FROM TABLE_DATE_RANGE(logs.requestlogs_, USEC_TO_TIMESTAMP(UTC_USEC_TO_WEEK(NOW(), 1)-1000000*60*60*24*14), CURRENT_TIMESTAMP())
-    WHERE REGEXP_MATCH(resource, '/api/internal/exercises/[x1-9a-f]+/edit') AND method = 'GET'
-    GROUP BY week) es
+    COUNTIF(status = 500) AS server_errors
+    FROM `logs.requestlogs_*`
+    WHERE
+        REGEXP_CONTAINS(resource, r'^/api/internal/exercises/[x0-9a-f]+/edit$')
+        AND method = 'GET'
+        AND _TABLE_SUFFIX BETWEEN 
+            FORMAT_DATE("%Y%m%d", DATE_SUB(CURRENT_DATE(), INTERVAL 14 DAY))
+            AND FORMAT_DATE("%Y%m%d", CURRENT_DATE())
+    GROUP BY week) editstatus
 
-ON edit.week = es.week
+ON edit.week = editstatus.week
 
-LEFT JOIN EACH
+LEFT JOIN
 
 (SELECT
-    STRFTIME_UTC_USEC(UTC_USEC_TO_WEEK(INTEGER(start_time*1000000), 1), "%Y-%m-%d") AS week,
-    NTH(5, QUANTILES(end_time-start_time,101)) AS p5_time,
-    NTH(25, QUANTILES(end_time-start_time,101)) AS q1_time,
-    NTH(50, QUANTILES(end_time-start_time,101)) AS median_time,
-    NTH(75, QUANTILES(end_time-start_time,101)) AS q3_time,
-    NTH(95, QUANTILES(end_time-start_time,101)) AS p95_time
-    FROM TABLE_DATE_RANGE(logs.requestlogs_, USEC_TO_TIMESTAMP(UTC_USEC_TO_WEEK(NOW(), 1)-1000000*60*60*24*14), CURRENT_TIMESTAMP())
-    WHERE resource CONTAINS '/api/internal/assessment_items/' AND method = "PUT" AND NOT (resource CONTAINS 'set_live') AND status = 200
+    FORMAT_DATE("%Y-%m-%d", DATE_TRUNC(DATE_FROM_UNIX_DATE(CAST(start_time/(60*60*24) AS INT64)), WEEK)) AS week,
+    APPROX_QUANTILES(end_time - start_time, 20) AS times
+    FROM `logs.requestlogs_*`
+    WHERE
+        resource LIKE '%/api/internal/assessment_items/%'
+        AND method = "PUT"
+        AND NOT (resource LIKE '%set_live%')
+        AND status = 200
+        AND _TABLE_SUFFIX BETWEEN 
+            FORMAT_DATE("%Y%m%d", DATE_SUB(CURRENT_DATE(), INTERVAL 14 DAY))
+            AND FORMAT_DATE("%Y%m%d", CURRENT_DATE())
     GROUP BY week) item
 
 ON edit.week = item.week
 
-LEFT JOIN EACH
+LEFT JOIN
 
 # Status for API endpoint for loading an exercise for editing
 (SELECT
-    STRFTIME_UTC_USEC(UTC_USEC_TO_WEEK(INTEGER(start_time*1000000), 1), "%Y-%m-%d") AS week,
+    FORMAT_DATE("%Y-%m-%d", DATE_TRUNC(DATE_FROM_UNIX_DATE(CAST(start_time/(60*60*24) AS INT64)), WEEK)) AS week,
     COUNT(*) AS attempts,
-    SUM(status = 500) AS server_errors,
-    FROM TABLE_DATE_RANGE(logs.requestlogs_, USEC_TO_TIMESTAMP(UTC_USEC_TO_WEEK(NOW(), 1)-1000000*60*60*24*14), CURRENT_TIMESTAMP())
-    WHERE resource CONTAINS '/api/internal/assessment_items/' AND method = "PUT" AND NOT (resource CONTAINS 'set_live')
-    GROUP BY week) is
+    COUNTIF(status = 500) AS server_errors
+    FROM `logs.requestlogs_*`
+    WHERE
+        resource LIKE '%/api/internal/assessment_items/%'
+        AND method = "PUT"
+        AND NOT (resource LIKE '%set_live%')
+        AND _TABLE_SUFFIX BETWEEN 
+            FORMAT_DATE("%Y%m%d", DATE_SUB(CURRENT_DATE(), INTERVAL 14 DAY))
+            AND FORMAT_DATE("%Y%m%d", CURRENT_DATE())
+    GROUP BY week) itemstatus
 
-ON edit.week = is.week
+ON edit.week = itemstatus.week
